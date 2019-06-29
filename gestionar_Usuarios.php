55022a62bed9af664460ce6f6b38c3354f159444
Kelly Aguilar <kelly.marie.aguilar@gmail.com>
2019-06-29 07:30:07 -0600

    Avance Index y gestión de usuarios

---
+++ gestionar_Usuarios.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+    <!-- Title -->
+    <title>VOAE | Orientación Profesional</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta charset="UTF-8">
+
+    <!-- Styles -->
+    <link type="text/css" rel="stylesheet" href="assets/plugins/materialize/css/materialize.min.css" />
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+
+
+    <!-- Theme Styles -->
+    <link href="assets/css/alpha.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/custom.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/gestionar_Usuarios.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="http://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="http://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+</head>
+
+<body>
+    <div id="div_loader"></div>
+
+    <div class="mn-content">
+
+        <div id="div-menu"></div>
+
+        <!--EN ESTE APARTADO VA TODO EL CONTENIDO QUE SE DESEA MOSTRAR EN LA SECCION PRINCIPAL-->
+        <main class="mn-inner">
+
+           
+
+
+
+            <div class="row">
+                <div class="col s12 m3">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="assets/images/profile-image.png" class="responsive-img" alt="">
+                        </div>
+                        <div class="card-title">
+                            <span>Diego Juan <br> Perez Chanfaina</span>
+                        </div>
+                        <div class="card-content">
+                            <div class="row info_list valign-wrapper">
+                                <i class="material-icons col s2 m2 l2">format_list_numbered</i>
+                                <span class="col s10 m10 l10">20151034449</span>
+                            </div>
+                            <div class="row info_list valign-wrapper">
+                                <i class="material-icons col s2 m2 l2">fingerprint</i>
+                                <span class="col s10 m10 l10">0801199804666</span>
+                            </div>
+                            <div class="row info_list valign-wrapper">
+                                <i class="material-icons col s2 m2 l2">import_contacts</i>
+                                <span class="col s10 m10 l10">Ingeniería en Sistemas</span>
+                            </div>
+                            <div class="row info_list valign-wrapper">
+                                <i class="material-icons col s2 m2 l2">email</i>
+                                <span class="col s10 m10 l10">juanito@unah.hn</span>
+                            </div>
+                            <div class="row info_list valign-wrapper">
+                                <i class="material-icons col s2 m2 l2">local_phone</i>
+                                <span class="col s10 m10 l10">32549875</span>
+                            </div>
+                            <div class="row info_list valign-wrapper">
+                                <i class="material-icons col s2 m2 l2">lock</i>
+                                <span class="col s10 m10 l10">Estudiante</span>
+                            </div>
+                        </div>
+                        <div class="card-action">
+                            <a class="red-text">Eliminar</a>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+
+
+
+
+
+        </main>
+        <!--FIN APARTADO-->
+
+        <div id="div-piePagina"></div>
+
+
+    </div>
+    <div class="left-sidebar-hover"></div>
+
+    <!-- Javascripts -->
+    <script src="assets/plugins/jquery/jquery-2.2.0.min.js"></script>
+    <script src="assets/plugins/materialize/js/materialize.min.js"></script>
+    <script src="assets/plugins/material-preloader/js/materialPreloader.min.js"></script>
+    <script src="assets/plugins/jquery-blockui/jquery.blockui.js"></script>
+    <script src="assets\plugins\prettify\prettify.js"></script>
+    <script src="assets/js/ContenidoFijo.js"></script>
+
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -18,6 +18,8 @@
     <!-- Theme Styles -->
     <link href="assets/css/alpha.css" rel="stylesheet" type="text/css" />
     <link href="assets/css/custom.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/index.css" rel="stylesheet" type="text/css" />
+    <link href="assets/plugins/vertical-timeline/css/style.css" rel="stylesheet">
 
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -29,24 +31,141 @@
 
 </head>
 
-<body class="loaded">
+<body>
+    <div id="div_loader"></div>
+
     <div class="mn-content">
 
         <div id="div-menu"></div>
-        
+
         <!--EN ESTE APARTADO VA TODO EL CONTENIDO QUE SE DESEA MOSTRAR EN LA SECCION PRINCIPAL-->
         <main class="mn-inner">
-            <div class="row">
-                <div class="col s12">
-                    <div class="page-title">Blank Page</div>
+            <div class="row valign-wrapper portada_landing">
+                <div class="col s12 m6 l5">
+                    <h2 class="cyan-text text-accent-4 scale-transition">Orientación Profesional</h2>
+                    <h5 class="cyan-text text-accent-4 scale-transition">Universidad Autónoma de Honduras</h5>
+                    <p>Esta plataforma ha sido creada como complemento al Proceso de Orientación Profesional que
+                        desarrolla el Área de Orientación y Asesoría Académica de la VOAE-UNAH. </p>
+                    <a
+                        class="waves-effect waves-light btn yellow darken-2 m-b-xs">Inscribirse</a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+
+                </div>
+                <div class="col s12 m6 l6 hide-on-small-only">
+                    <img src="assets/images/personal_landing_Page.svg" class="responsive-img" alt=""
+                        id="img_cropper_foto">
                 </div>
             </div>
+
+      
+            <div class="row">
+                <div class="col s12">
+                <h4 class="cyan-text text-accent-4 scale-transition">Descripción del Proceso</h4>
+                </div>
+                <div class="col s12">
+
+                    <section id="cd-timeline" class="cd-container">
+                        <div class="cd-timeline-block">
+                            <div class="cd-timeline-img cd-picture valign-wrapper amber darken-1">
+                                <i class="material-icons white-text center" style="width: 100%;">people</i>
+                            </div> <!-- cd-timeline-img -->
+
+                            <div class="cd-timeline-content">
+                                <h2>Evaluación Grupal</h2>
+                                <div class="row">
+                                    <i class="material-icons cyan-text text-darken-3 col s2 m1 l1">check</i>
+                                    <h6 class="col s10 m11 l11 left-align">Evaluación de la capacidad intelectual.</h6>
+
+                                    <i class="material-icons cyan-text text-darken-3 col s2 m1 l1">check</i>
+                                    <h6 class="col s10 m11 l11 left-align">Evaluación de aptitudes.</h6>
+                                </div>
+                                <span class="cd-date">Primer Etapa</span>
+                            </div> <!-- cd-timeline-content -->
+                        </div> <!-- cd-timeline-block -->
+
+                        <div class="cd-timeline-block">
+                            <div class="cd-timeline-img cd-picture valign-wrapper  lime accent-4">
+                                <i class="material-icons white-text center" style="width: 100%;">dvr</i>
+                            </div> <!-- cd-timeline-img -->
+
+                            <div class="cd-timeline-content">
+                                <h2>Test en Línea</h2>
+                                <div class="row">
+                                    <i class="material-icons cyan-text text-darken-3 col s2 m1 l1">check</i>
+                                    <h6 class="col s10 m11 l11 left-align">Evaluación de personalidad e intereses
+                                        voacionales.</h6>
+                                </div>
+                                <span class="cd-date">Segunda Etapa</span>
+                            </div> <!-- cd-timeline-content -->
+                        </div> <!-- cd-timeline-block -->
+
+                        <div class="cd-timeline-block">
+                            <div class="cd-timeline-img cd-picture valign-wrapper teal accent-4">
+                                <i class="material-icons white-text center" style="width: 100%;">forum</i>
+                            </div> <!-- cd-timeline-img -->
+
+                            <div class="cd-timeline-content">
+                                <h2>Entrevista Pedagógica</h2>
+                                <div class="row">
+                                    <i class="material-icons cyan-text text-darken-3 col s2 m1 l1">check</i>
+                                    <h6 class="col s10 m11 l11 left-align">Entrevista de aspectos relacionados con
+                                        trayectoria académica así como hábitos y técnicas de estudio.</h6>
+
+                                    <i class="material-icons cyan-text text-darken-3 col s2 m1 l1">check</i>
+                                    <h6 class="col s10 m11 l11 left-align">Recomendaciones de aspectos pedagógicos.</h6>
+                                </div>
+                                <span class="cd-date">Tercera Etapa</span>
+                            </div> <!-- cd-timeline-content -->
+                        </div> <!-- cd-timeline-block -->
+
+                        <div class="cd-timeline-block">
+                            <div class="cd-timeline-img cd-picture valign-wrapper red darken-1">
+                                <i class="material-icons white-text center" style="width: 100%;">event</i>
+                            </div> <!-- cd-timeline-img -->
+
+                            <div class="cd-timeline-content">
+                                <h2>Devolución de Resultados</h2>
+                                <div class="row">
+                                    <i class="material-icons cyan-text text-darken-3 col s2 m1 l1">check</i>
+                                    <h6 class="col s10 m11 l11 left-align">Explicación y fundamentos e cada prueba.</h6>
+
+                                    <i class="material-icons cyan-text text-darken-3 col s2 m1 l1">check</i>
+                                    <h6 class="col s10 m11 l11 left-align">Conclusiones y recomendaciones por parte del
+                                        psicólogo.</h6>
+                                </div>
+                                <span class="cd-date">Cuarta Etapa</span>
+                            </div> <!-- cd-timeline-content -->
+                        </div> <!-- cd-timeline-block -->
+
+
+                    </section> <!-- cd-timeline -->
+
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         </main>
         <!--FIN APARTADO-->
 
         <div id="div-piePagina"></div>
 
-        
+
     </div>
     <div class="left-sidebar-hover"></div>
 
@@ -55,8 +174,11 @@
     <script src="assets/plugins/materialize/js/materialize.min.js"></script>
     <script src="assets/plugins/material-preloader/js/materialPreloader.min.js"></script>
     <script src="assets/plugins/jquery-blockui/jquery.blockui.js"></script>
-    
-    <script src="assets/js/menuPiePagina.js"></script>
+    <script src="assets\plugins\prettify\prettify.js"></script>
+    <script src="assets/js/pages/ui-parallax.js"></script>
+    <script src="assets/plugins/vertical-timeline/js/modernizr.js"></script>
+    <script src="assets/js/ContenidoFijo.js"></script>
+    <script src="assets/js/index.js"></script>
 
 </body>
 
