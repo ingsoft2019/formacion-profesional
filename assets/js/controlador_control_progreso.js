9de197975b601e644f6a1da9241128de0cdd0515
Kelly Aguilar <kelly.marie.aguilar@gmail.com>
2019-08-20 11:38:06 -0600

    Control de Progreso
    Interfaz

---
+++ assets/js/controlador_control_progreso.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+    $(".frm_etapas").click(function(event) {
+        var datosEtapa = [];
+        if (event.target.type == "checkbox") {
+            var id_proceso = event.target.getAttribute('data-process');
+            var id_user = event.target.getAttribute('data-user');
+            var id_etapa = event.target.getAttribute('id');
+            var estado_etapa = $("#" + id_etapa).is(':checked');
+            var progreso = parseInt($("#porcentaje_user" + id_user).text());
+            if (estado_etapa) {
+                progreso += 25;
+            } else {
+                progreso -= 25;
+            }
+
+            datosEtapa["id_proceso"] = id_proceso;
+            datosEtapa["id_user"] = id_user;
+            datosEtapa["id_etapa"] = id_etapa;
+            datosEtapa["estado_etapa"] = estado_etapa;
+            datosEtapa["progreso"] = progreso;
+
+            $("#porcentaje_user" + id_user).text(progreso);
+            $('#bar_user' + id_user).css('width', progreso + "%");
+
+            console.log(datosEtapa);
+            /*console.log(id_proceso);
+            console.log(id_user);
+            console.log(id_etapa);
+            console.log(estado_etapa);*/
+        }
+    });
+
+
+});
--- control_progresos.php
+++ control_progresos.php
@@ -28,6 +28,7 @@
     <link href="assets/plugins/sweetalert/sweetalert.css" rel="stylesheet" type="text/css" />
     <link href="assets/plugins/material-preloader/css/materialPreloader.min.css" rel="stylesheet">
     <link rel="icon" type="image/png" href="assets\images\icon.png" />
+    <link href="assets/css/control_progreso.css" rel="stylesheet" type="text/css" />
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -47,15 +48,97 @@
         <!--EN ESTE APARTADO VA TODO EL CONTENIDO QUE SE DESEA MOSTRAR EN LA SECCION PRINCIPAL-->
         <main class="mn-inner">
 
+            <div class="row">
+
+                <div class="col s12 m6 l4">
+                    <div class="card">
+                        <div class="card-content">
+                            <div class="row center-align">
+                                <h5>Juana Maria <br>
+                                    Amador Pérez</h5>
+                                <h6>20151023698</h6>
+                                <h6>Ingeniería en Sistemas</h6>
+                            </div>
+                            <div class="row" style="margin-bottom:5px;">
+                                <div class="col s12">
+                                    <div class="row valign-wrapper" style="margin-bottom:5px;">
+                                        <h6 class="col s10">
+                                            Porcentaje Completado <br>
+                                            <strong>Proceso No. 7</strong>
+                                        </h6>
+                                        <h6 class="col s2 right-align">
+                                            <span id="porcentaje_user12">50</span>%
+                                        </h6>
+                                    </div>
+                                    <div class="progress ">
+                                        <div class="determinate" style="width: 50%" id="bar_user12"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row center-align activator">
+                                <i class="material-icons center" style="cursor:pointer;">more_horiz</i>
+                            </div>
+                        </div>
+                        <div class="card-reveal">
+                            <div class="row">
+                                <span class="card-title grey-text text-darken-4">
+                                    <i class="material-icons right">close</i>
+                                </span>                                
+                                <div class="col s12">
+                                <br>
+                                    <form class="frm_etapas" action="#">
+                                        <p class="p-v-xs">
+                                            <input type="checkbox" class="filled-in" id="e1_12" checked="checked"
+                                                data-process="7" data-user="12" />
+                                            <label for="e1_12">
+                                                Evaluación Grupal
+                                            </label>
+                                        </p>
+                                        <p class="p-v-xs">
+                                            <input type="checkbox" class="filled-in" id="e2_12" checked="checked"
+                                                data-process="7" data-user="12" />
+                                            <label for="e2_12">
+                                                Evaluación en Línea
+                                            </label>
+                                        </p>
+                                        <p class="p-v-xs">
+                                            <input type="checkbox" class="filled-in" id="e3_12" data-process="7"
+                                                data-user="12" />
+                                            <label for="e3_12">
+                                                Entrevista Pedagógica
+                                            </label>
+                                        </p>
+                                        <p class="p-v-xs">
+                                            <input type="checkbox" class="filled-in" id="e4_12" data-process="7"
+                                                data-user="12" />
+                                            <label for="e4_12">
+                                                Devolución de Resultados
+                                            </label>
+                                        </p>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
 
 
 
-        </main>
-        <!--FIN APARTADO-->
 
 
 
-        <div id="div-piePagina"></div>
+
+    </div>
+
+
+    </main>
+    <!--FIN APARTADO-->
+
+
+
+    <div id="div-piePagina"></div>
 
 
     </div>
@@ -72,7 +155,7 @@
     <script src="assets/js/ContenidoFijo.js"></script>
     <script src="assets/plugins/jquery-inputmask/jquery.inputmask.bundle.js"></script>
     <script src="assets/js/pages/form-input-mask.js"></script>
-    <script src="assets/js/gestionar_Usuarios.js"></script>
+    <script src="assets/js/controlador_control_progreso.js"></script>
 
 </body>
 
