3640b7262b6ca176c3510ba64b1c447be0e3eff6
Alejandro Zelaya <jandro.zg123@gmail.com>
2019-08-28 16:42:58 -0600

    cambios una tabla
    en la tabla tbl_horarios_orientador el tipo de evento se guardo como string y el sistema lo guarda como numero, asi q hay q cambiarlo a numeros porq es mas facil

---
+++ Base de Datos/changes2.sql
@@ -0,0 +1,15 @@
+UPDATE  tbl_horarios_orientador
+SET     tipoevento = IF(tipoevento = 'Entrevista', 2 , 3)
+where idhorariosorientador=1;
+UPDATE  tbl_horarios_orientador
+SET     tipoevento = IF(tipoevento = 'Entrevista', 2 , 3)
+where idhorariosorientador=2;
+UPDATE  tbl_horarios_orientador
+SET     tipoevento = IF(tipoevento = 'Entrevista', 2 , 3)
+where idhorariosorientador=3;
+UPDATE  tbl_horarios_orientador
+SET     tipoevento = IF(tipoevento = 'Entrevista', 2 , 3)
+where idhorariosorientador=4;
+UPDATE  tbl_horarios_orientador
+SET     tipoevento = IF(tipoevento = 'Entrevista', 2 , 3)
+where idhorariosorientador=5;
